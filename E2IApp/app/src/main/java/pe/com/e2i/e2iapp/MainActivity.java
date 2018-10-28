a57bcb9c2ae3956749542e554e1ca0f5b38276d2
Eduzc07 <edu.zc07@gmail.com>
2018-10-28 16:57:38 +0100

    Settings have been added.

--- E2IApp/app/src/main/java/pe/com/e2i/e2iapp/MainActivity.java
+++ E2IApp/app/src/main/java/pe/com/e2i/e2iapp/MainActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity implements MainFragment.Callback {
 
@@ -32,6 +35,30 @@ public class MainActivity extends AppCompatActivity implements MainFragment.Call
 //        TextView tv = (TextView) findViewById( R.id.sample_text );
 //        tv.setText( stringFromJNI() );
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     /**
